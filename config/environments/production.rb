dfee81bd982473746ab33483353a38581f8d6258
Natalie Barba <nbarba2010@gmail.com>
2022-01-14 14:03:57 -0800

    changed host settings for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,8 +58,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp 
-  host = "localhost:3000"
-  config.action_mailer.default_url_options = { host: "localhost:3000", protocol: "http" }
+  config.action_mailer.default_url_options = { host: "just-average-cooks.herokuapp.com" }
 
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
